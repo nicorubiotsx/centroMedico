7fe9cc5be0b3338fedfd580b523d46b93e740464
Nicolás Rubio <nrubioolivares@gmail.com>
2026-06-30 17:11:20 -0400

    feat: rediseño menu hamburguesa 2.0

--- components/ThemeContext.js
+++ components/ThemeContext.js
@@ -5,7 +5,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState('vanguardista'); // 'vanguardista' (default), 'confianza'
+  const [theme, setTheme] = useState('confianza'); // 'confianza' (default light), 'vanguardista' (dark)
 
   useEffect(() => {
     // Cargar el tema inicial desde localStorage si existe
